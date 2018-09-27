bf46a46f3b713f7426247e0d832ae4974f1f70dc
Andy Guo <andy.guo@ixdigit.com>
2018-09-27 11:35:21 +0800

    demo

--- app/src/main/java/com/example/rtmp/rtmp/MainActivity.java
+++ app/src/main/java/com/example/rtmp/rtmp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rtmp.rtmp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import io.vov.vitamio.LibsChecker;
 import io.vov.vitamio.MediaPlayer;
@@ -14,19 +16,22 @@ public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
     private String path = "";
     private VideoView mVideoView;
+    private WebView webview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(!LibsChecker.checkVitamioLibs(this)){
-            Log.e(TAG,"Libs not complete!!");
+        if (!LibsChecker.checkVitamioLibs(this)) {
+            Log.e(TAG, "Libs not complete!!");
             return;
         }
         setContentView(R.layout.activity_main);
 
-        mVideoView = (VideoView)findViewById(R.id.vitamio_videoView);
+        mVideoView = (VideoView) findViewById(R.id.vitamio_videoView);
         path = "rtmp://218.63.117.136:8093/hls/camera";
         mVideoView.setVideoPath(path);
-        mVideoView.setMediaController(new MediaController(this));
+        //mVideoView.setMediaController(new MediaController(this));
+        mVideoView.setBufferSize(100 * 1024);
         mVideoView.requestFocus();
         mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
@@ -35,5 +40,34 @@ public class MainActivity extends Activity {
             }
         });
         mVideoView.start();
+
+        webview = (WebView)findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        webSettings.setJavaScriptEnabled(true);
+        //支持插件
+        //webSettings.setPluginsEnabled(true);
+
+        //设置自适应屏幕，两者合用
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+        webSettings.setDomStorageEnabled(true);
+        //缩放操作
+        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        webSettings.setDomStorageEnabled(true);
+
+        //其他细节操作WebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+//        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webSettings.setAllowFileAccess(true); //设置可以访问文件
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+
+        webview.setBackgroundColor(0); // 设置背景色
+        //webview.getBackground().setAlpha(0);
+        webview.loadUrl("file:///android_asset/demo.html");
+
     }
 }
